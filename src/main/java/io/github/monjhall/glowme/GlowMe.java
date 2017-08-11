8f2713a8cafde47c561cf7626dcc0c4766b60982
Thomas Lourenco <TSLourenco@WPI.edu>
2017-08-11 10:50:38 -0400

    Added onEnable() and onDisable()
    Added the two mentioned methods with logging when invoked. Currently
    causes an error when being run.

--- src/main/java/io/github/monjhall/glowme/GlowMe.java
+++ src/main/java/io/github/monjhall/glowme/GlowMe.java
@@ -4,4 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class GlowMe extends JavaPlugin {
 
+	@Override
+	public void onEnable() {
+		// This method should contain all the logic to be performed when the plugin is enabled.
+		
+		// Simple log of onEnable being invoked.
+		getLogger().info("onEnable has been invoked!");
+	}
+	
+	@Override 
+	public void onDisable() {
+		// This method should contain all the logic to be performed when the plugin is enabled.
+		
+		// Simple log of onDisable being invoked.
+		getLogger().info("onDisable has been invoked!");
+	}
 }
