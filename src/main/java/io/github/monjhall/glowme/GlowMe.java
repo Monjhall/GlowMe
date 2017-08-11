5ac776053c60dc2788f3ff39d9b6de77f3c78bce
Thomas Lourenco <TSLourenco@WPI.edu>
2017-08-11 12:02:21 -0400

    Added a command that always executes and logs that it properly ran.
    Used onCommand to add a /glow command that contains no functionality at
    the moment.
    Updated plugin version to v0.1.1

--- src/main/java/io/github/monjhall/glowme/GlowMe.java
+++ src/main/java/io/github/monjhall/glowme/GlowMe.java
@@ -1,5 +1,7 @@
 package io.github.monjhall.glowme;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class GlowMe extends JavaPlugin {
@@ -18,5 +20,23 @@ public class GlowMe extends JavaPlugin {
 		
 		// Simple log of onDisable being invoked.
 		getLogger().info("onDisable has been invoked!");
+	}
+	
+	@Override
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		// If using the command glow...
+		if (cmd.getName().equalsIgnoreCase("glow")) {
+			
+			// TODO: Fill out command functionality.
+			getLogger().info("Command used properly...");
+			
+			return true;
+		}
+		
+		// Return false if the function failed.
+		getLogger().info("Command used improperly...");
+		return false;
+		
 	}
 }
