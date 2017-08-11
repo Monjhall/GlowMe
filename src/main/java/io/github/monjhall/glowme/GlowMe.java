138590823e325572606b76e451cc384e24d13c9d
Thomas Lourenco <TSLourenco@WPI.edu>
2017-08-11 18:03:20 -0400

    Added full functionality to the glow command.
    Updated plugin version to 0.1.5.
    Added full functionality for the glow command.
    Need to fix a bug where player's teams aren't reset after the glow
    duration expires.

--- src/main/java/io/github/monjhall/glowme/GlowMe.java
+++ src/main/java/io/github/monjhall/glowme/GlowMe.java
@@ -1,10 +1,14 @@
 package io.github.monjhall.glowme;
 
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+import org.bukkit.scoreboard.*;
 
 public class GlowMe extends JavaPlugin {
 
@@ -30,26 +34,124 @@ public class GlowMe extends JavaPlugin {
 		// If using the command glow...
 		if (cmd.getName().equalsIgnoreCase("glow")) {
 			
-			if (args.length != 3) {
-				sender.sendMessage("Wrong number of arguments.");
+			// If there are less than 2 arguments, return false.
+			if (args.length < 2) {
+				sender.sendMessage("Too few arguments, proper usage below.");
 				return false;
 			}
 			
-			Player target = (Bukkit.getServer().getPlayer(args[0]));
-			if (target == null) {
-				sender.sendMessage(args[0] + " is not online!");
-		        return false;
+			// If there are only 2 arguments, set the glow on the sender.
+			if (args.length == 2) {
+				
+				// If the sender is the console, return false.
+				if (!(sender instanceof Player)) {
+					sender.sendMessage("Using the command through the console requires all three arguments.");
+					return false;
+				}
+				
+				// If the sender is a player, set the glow on the sender.
+				
+				// Verify duration is an integer, otherwise catch the exception.
+				int duration;
+				try {
+					duration = Integer.parseInt(args[0]);
+				} catch (NumberFormatException e) {
+					sender.sendMessage("Duration must be a number, proper usage below.");
+					return false;
+				}
+				
+				// Verify that a proper color was provided.
+				ChatColor teamColor;
+				try {
+					teamColor = ChatColor.valueOf(args[1]);
+				} catch (IllegalArgumentException e) {
+					sender.sendMessage("Color must be one of the allowed scoreboard colors.");
+					return false;
+				}
+				
+				if (teamColor.isFormat()) {
+					sender.sendMessage("Color must be a color, not a format code.");
+				}
+				
+				setGlow((Player) sender, duration * 20, teamColor);
 			}
 			
-			// TODO: Fill out command functionality.
-			sender.sendMessage("Functionality needs to be added to the method body.");
+			// If there are three arguments, set the glow on the specified player.
+			if (args.length == 3) {
+				
+				// Record the target player.
+				Player target = (Bukkit.getServer().getPlayer(args[0]));
+				
+				// If the target is not online, return false!
+				if (target == null) {
+					sender.sendMessage(args[0] + " is not online!");
+			        return false;
+				}
+				
+				// Verify duration is an integer, otherwise catch the exception.
+				int duration;
+				try {
+					duration = Integer.parseInt(args[1]);
+				} catch (NumberFormatException e) {
+					sender.sendMessage("Duration must be a number, proper usage below.");
+					return false;
+				}
+				
+				// Verify that a proper color was provided.
+				ChatColor teamColor;
+				try {
+					teamColor = ChatColor.valueOf(args[2]);
+				} catch (IllegalArgumentException e) {
+					sender.sendMessage("Color must be one of the allowed scoreboard colors.");
+					return false;
+				}
+				
+				if (teamColor.isFormat()) {
+					sender.sendMessage("Color must be a color, not a format code.");
+				}
+				
+				setGlow(target, duration * 20, teamColor);
+				
+			}
+			
+			// If there are more than three arguments, return false.
+			if (args.length > 3) {
+				sender.sendMessage("Too many arguments, proper usage below.");
+				return false;
+			}
 			
 			return true;
 		}
 		
 		// Return false if the function failed.
-		getLogger().info("Command used improperly...");
+		getLogger().info("Failure reached after argument checking.");
 		return false;
 		
+	}
+	
+	private void setGlow(Player player, int duration, ChatColor color) {
+		
+		// Create the glow effect type.
+		PotionEffectType glowEffectType = PotionEffectType.getById(24);
+		
+		// Create the glow effect.
+		PotionEffect glowEffect = new PotionEffect(glowEffectType, duration, 1);
+		
+		// Get the scoreboard manager, create a scoreboard, and set the new teams.
+		ScoreboardManager manager = Bukkit.getScoreboardManager();
+		Scoreboard mainBoard = manager.getMainScoreboard();
+		
+		// Check if the specific colored team already exists.
+		Team playerTeam;
+		if (mainBoard.getTeam(color.toString()) == null) {
+			playerTeam = mainBoard.registerNewTeam(color.toString());
+		} else {
+			playerTeam = mainBoard.getTeam(color.toString());
+		}
+		playerTeam.addPlayer(player);
+		playerTeam.setPrefix(color + "");
+		
+		// Apply the glow to the provided player.
+		glowEffect.apply(player);
 	}
 }
