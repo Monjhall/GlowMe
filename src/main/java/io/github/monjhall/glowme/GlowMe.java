324242ef0a74d1bc10d16a7d9f39e1c192db1481
Thomas Lourenco <TSLourenco@WPI.edu>
2017-08-13 11:32:17 -0400

    General plugin clean up and big fixes.
    Removed the onCommand function that was commented out in GlowMe.java.
    Removed unused imports (Command/CommandSender).
    Set glow now removes an active glow before applying the new one.
    Updated the plugin version to 0.3.4.

--- src/main/java/io/github/monjhall/glowme/GlowMe.java
+++ src/main/java/io/github/monjhall/glowme/GlowMe.java
@@ -2,14 +2,9 @@ package io.github.monjhall.glowme;
 
 import java.util.Collection;
 import java.util.Iterator;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -74,162 +69,6 @@ public class GlowMe extends JavaPlugin {
 		// This method should contain all the logic to be performed when the plugin is disabled.
 	}
 	
-	/*
-	@Override
-	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		
-		// If using the command glow...
-		if (cmd.getName().equalsIgnoreCase("glow")) {
-			
-			// If there are less than 2 arguments, return false.
-			if (args.length < 2) {
-				sender.sendMessage("Too few arguments, proper usage below.");
-				return false;
-			}
-			
-			// If there are only 2 arguments, set the glow on the sender.
-			if (args.length == 2) {
-				
-				// If the sender is the console, return false.
-				if (!(sender instanceof Player)) {
-					sender.sendMessage("Using the command through the console requires all three arguments.");
-					return false;
-				}
-				
-				// If the sender is a player, set the glow on the sender.
-				
-				// Verify duration is an integer, otherwise catch the exception.
-				int duration;
-				try {
-					duration = Integer.parseInt(args[0]);
-				} catch (NumberFormatException e) {
-					sender.sendMessage("Duration must be a number, proper usage below.");
-					return false;
-				}
-				
-				// Verify that a proper color was provided.
-				ChatColor teamColor;
-				try {
-					teamColor = ChatColor.valueOf(args[1].toUpperCase());
-				} catch (IllegalArgumentException e) {
-					sender.sendMessage("Color must be one of the allowed scoreboard colors.");
-					return false;
-				}
-				
-				if (teamColor.isFormat()) {
-					sender.sendMessage("Color must be a color, not a format code.");
-					return false;
-				}
-				
-				setGlow((Player) sender, duration * 20, teamColor);
-			}
-			
-			// If there are three arguments, set the glow on the specified player.
-			if (args.length == 3) {
-				
-				// Record the target player.
-				Player target = (Bukkit.getServer().getPlayer(args[0]));
-				
-				// If the target is not online, return false!
-				if (target == null) {
-					sender.sendMessage(args[0] + " is not online!");
-			        return false;
-				}
-				
-				// Verify duration is an integer, otherwise catch the exception.
-				int duration;
-				try {
-					duration = Integer.parseInt(args[1]);
-				} catch (NumberFormatException e) {
-					sender.sendMessage("Duration must be a number, proper usage below.");
-					return false;
-				}
-				
-				// Verify that a proper color was provided.
-				ChatColor teamColor;
-				try {
-					teamColor = ChatColor.valueOf(args[2].toUpperCase());
-				} catch (IllegalArgumentException e) {
-					sender.sendMessage("Color must be one of the allowed scoreboard colors.");
-					return false;
-				}
-				
-				if (teamColor.isFormat()) {
-					sender.sendMessage("Color must be a color, not a format code.");
-					return false;
-				}
-				
-				setGlow(target, duration * 20, teamColor);
-				
-			}
-			
-			// If there are more than three arguments, return false.
-			if (args.length > 3) {
-				sender.sendMessage("Too many arguments, proper usage below.");
-				return false;
-			}
-			
-			return true;
-		}
-		
-		// If using the command clearglow...
-		if (cmd.getName().equalsIgnoreCase("clearglow")) {
-			if (args.length == 0) {
-				
-				// If the sender is the console, return false.
-				if (!(sender instanceof Player)) {
-					sender.sendMessage("Using the command through the console requires a specified player.");
-					return false;
-				}
-				
-				// If the player has no glow, warn the user.
-				if (!(((Player) sender).hasPotionEffect(PotionEffectType.getById(24)))) {
-					sender.sendMessage("You have no active glow!");
-					return false;
-				}
-				
-				// Otherwise, clear their glow.
-				clearGlow((Player) sender);
-			}
-			
-			if (args.length == 1) {
-				
-				// Record the target player.
-				Player target = (Bukkit.getServer().getPlayer(args[0]));
-				
-				// If the target is not online, return false!
-				if (target == null) {
-					sender.sendMessage(args[0] + " is not online!");
-			        return false;
-				}
-				
-				// If the player has no glow, warn the user.
-				if (!(((Player) sender).hasPotionEffect(PotionEffectType.getById(24)))) {
-					sender.sendMessage(args[0] + " has no active glow!");
-					return false;
-				}
-				
-				// Otherwise, clear that player's glow.
-				clearGlow(target);
-				
-			}
-			
-			// If there is more than one arguments, return false.
-			if (args.length > 1) {
-				sender.sendMessage("Too many arguments, proper usage below.");
-				return false;
-			}
-			
-			return true;
-		}
-		
-		// Return false if the function failed.
-		getLogger().info("No command found.");
-		return false;
-		
-	}
-	*/
-	
 	protected void setGlow(Player player, int duration, ChatColor color) {
 		
 		// Create the glow effect type.
@@ -237,6 +76,12 @@ public class GlowMe extends JavaPlugin {
 		
 		// Create the glow effect.
 		PotionEffect glowEffect = new PotionEffect(glowEffectType, duration, 1);
+		
+		// If the player has a glow effect already, remove it.
+		if (player.hasPotionEffect(PotionEffectType.getById(24))) {
+			player.removePotionEffect(PotionEffectType.getById(24));
+			removePlayerFromTeam(player);
+		}
 		
 		// Add the player to the specified team.
 		addPlayerToTeam(player, color);
