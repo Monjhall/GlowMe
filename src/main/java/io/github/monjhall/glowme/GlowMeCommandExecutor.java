ad1b765878d4e791e72c7195f26983636e870e21
Thomas Lourenco <TSLourenco@WPI.edu>
2017-08-13 11:21:56 -0400

    Updated plugin to include clear glow command and more!
    Added clearglow command.
    Created a new GlowMeCommandExecutor class to handle commands.
    Found a bug where new glow commands don't update glow duration.

--- src/main/java/io/github/monjhall/glowme/GlowMeCommandExecutor.java
+++ src/main/java/io/github/monjhall/glowme/GlowMeCommandExecutor.java
@@ -1,80 +1,21 @@
 package io.github.monjhall.glowme;
 
-import java.util.Collection;
-import java.util.Iterator;
-
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
-import org.bukkit.scheduler.BukkitScheduler;
-import org.bukkit.scoreboard.*;
 
-public class GlowMe extends JavaPlugin {
+public class GlowMeCommandExecutor implements CommandExecutor {
+	private final GlowMe plugin;
+	
+	// Constructor.
+	public GlowMeCommandExecutor(GlowMe plugin) {
+		this.plugin = plugin;
+	}
 
-	@Override
-	public void onEnable() {
-		
-		// This method should contain all the logic to be performed when the plugin is enabled.
-		
-		// Set the commands to be executed by the command executor.
-		GlowMeCommandExecutor commandExecutor = new GlowMeCommandExecutor(this);
-		this.getCommand("glow").setExecutor(commandExecutor);
-		this.getCommand("clearglow").setExecutor(commandExecutor);
-		
-		// Create a scheduler to run tasks.
-		BukkitScheduler scheduler = getServer().getScheduler();
-		
-		// Schedule a task to be repeatedly run and create it.
-		// This task will check if player's need to be removed from teams, since their glow is running out.
-		scheduler.scheduleSyncRepeatingTask(this, new Runnable() {
-			
-			@Override
-			public void run() {
-				
-				// Collection of all online players.
-				Collection<Player> onlinePlayers = (Collection<Player>) Bukkit.getServer().getOnlinePlayers();
-				
-				// Iterate through the collection of players, looking at their active effects.
-				for (Iterator<Player> playerIterator = onlinePlayers.iterator();
-						playerIterator.hasNext();) {
-					
-					// Create a collection of the current player's active effects.
-					Player activePlayer = playerIterator.next();
-					Collection<PotionEffect> playerActiveEffects = activePlayer.getActivePotionEffects();
-					
-					// If the active player isn't glowing, ignore them.
-					if (!(activePlayer.hasPotionEffect(PotionEffectType.getById(24)))) continue;
-					
-					// If they are glowing, check the time on the effect.
-					for (Iterator<PotionEffect> effectsIterator = playerActiveEffects.iterator();
-							effectsIterator.hasNext();) {
-						
-						// Verify the active effect is the glow.
-						PotionEffect activeEffect = effectsIterator.next();
-						if (activeEffect.getType() != PotionEffectType.getById(24)) continue;
-						
-						// If the glow is about to run out, remove the player from the team.
-						if (activeEffect.getDuration() <= 15) removePlayerFromTeam(activePlayer);
-						
-					}
-				}
-			}
-		}, 0L, 10L);
-	}
-	
-	@Override 
-	public void onDisable() {
-		// This method should contain all the logic to be performed when the plugin is disabled.
-	}
-	
-	/*
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 		
@@ -121,7 +62,7 @@ public class GlowMe extends JavaPlugin {
 					return false;
 				}
 				
-				setGlow((Player) sender, duration * 20, teamColor);
+				plugin.setGlow((Player) sender, duration * 20, teamColor);
 			}
 			
 			// If there are three arguments, set the glow on the specified player.
@@ -159,7 +100,7 @@ public class GlowMe extends JavaPlugin {
 					return false;
 				}
 				
-				setGlow(target, duration * 20, teamColor);
+				plugin.setGlow(target, duration * 20, teamColor);
 				
 			}
 			
@@ -189,7 +130,7 @@ public class GlowMe extends JavaPlugin {
 				}
 				
 				// Otherwise, clear their glow.
-				clearGlow((Player) sender);
+				plugin.clearGlow((Player) sender);
 			}
 			
 			if (args.length == 1) {
@@ -210,7 +151,7 @@ public class GlowMe extends JavaPlugin {
 				}
 				
 				// Otherwise, clear that player's glow.
-				clearGlow(target);
+				plugin.clearGlow(target);
 				
 			}
 			
@@ -224,60 +165,8 @@ public class GlowMe extends JavaPlugin {
 		}
 		
 		// Return false if the function failed.
-		getLogger().info("No command found.");
+		plugin.getLogger().info("No command found.");
 		return false;
-		
 	}
-	*/
-	
-	protected void setGlow(Player player, int duration, ChatColor color) {
-		
-		// Create the glow effect type.
-		PotionEffectType glowEffectType = PotionEffectType.getById(24);
-		
-		// Create the glow effect.
-		PotionEffect glowEffect = new PotionEffect(glowEffectType, duration, 1);
-		
-		// Add the player to the specified team.
-		addPlayerToTeam(player, color);
-		
-		// Apply the glow to the provided player.
-		glowEffect.apply(player);
-	}
-	
-	protected void clearGlow(Player player) {
-		
-		if (player.hasPotionEffect(PotionEffectType.getById(24))) {
-			player.removePotionEffect(PotionEffectType.getById(24));
-			removePlayerFromTeam(player);
-		}
-		
-	}
-	
-	private void removePlayerFromTeam(Player player) {
-		
-		// Get the scoreboard manager, get the main scoreboard, and set the new teams.
-		ScoreboardManager manager = Bukkit.getScoreboardManager();
-		Scoreboard mainBoard = manager.getMainScoreboard();
-		Team playerTeam = mainBoard.getPlayerTeam(player);
-		playerTeam.removePlayer(player);
-	}
-	
-	private void addPlayerToTeam(Player player, ChatColor color) {
-		
-		// Get the scoreboard manager, create a scoreboard, and set the new teams.
-		ScoreboardManager manager = Bukkit.getScoreboardManager();
-		Scoreboard mainBoard = manager.getMainScoreboard();
-		
-		// Check if the specific colored team already exists.
-		Team playerTeam;
-		if (mainBoard.getTeam(color.toString()) == null) {
-			playerTeam = mainBoard.registerNewTeam(color.toString());
-		} else {
-			playerTeam = mainBoard.getTeam(color.toString());
-		}
-		playerTeam.addPlayer(player);
-		playerTeam.setPrefix(color + "");
-	}
-	
+
 }
